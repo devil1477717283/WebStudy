334b37eee56bbe397eac2ecf31c30b2d3249bec4
Tohsaka_evil <1477717283@qq.com>
2022-04-19 16:41:12 +0800

    fixed bug

--- JavaScriptDemo/Project-Guess Number/script.js
+++ JavaScriptDemo/Project-Guess Number/script.js
@@ -10,16 +10,26 @@ document.querySelector('.check').addEventListener('click',function(){
         if(guess>guessNmuber){
             document.querySelector('.message').textContent='To High';
             score--;
+            if(score<=0){
+                document.querySelector('.message').textContent='You losed the game';
+                score=0;
+            }
             document.querySelector('.score').textContent=score;
         }else if(guess<guessNmuber){
             document.querySelector('.message').textContent='To Low';
             score--;
+            if(score<=0){
+                document.querySelector('.message').textContent='You losed the game';
+                score=0;
+            }
             document.querySelector('.score').textContent=score;
         }else{
             document.querySelector('.message').textContent='Correct Number!';
             if(historyMaxSocre<score){
                 historyMaxSocre=score;
             }
+            document.querySelector('body').style.backgroundColor='#60b347';
+            document.querySelector('.number').style.width='30rem';
             document.querySelector('.number').textContent=guessNmuber;
             document.querySelector('.highscore').textContent=historyMaxSocre;
         }
@@ -27,6 +37,8 @@ document.querySelector('.check').addEventListener('click',function(){
 });
 document.querySelector('.again').addEventListener('click', function(){
     guessNmuber=Math.trunc(Math.random()*20)+1
+    document.querySelector('body').style.backgroundColor='#222'
+    document.querySelector('.number').style.width='15rem';
     document.querySelector('.number').textContent='?';
     score=20;
     document.querySelector('.score').textContent=score;
